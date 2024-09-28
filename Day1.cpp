c9cc9fffa62b6f6105a43bef87a5431571212f13
Harsh_Tripathi <144989081+alwaysharshtripathi@users.noreply.github.com>
2024-09-28 22:44:05 +0530

    Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int points;
+    cin>>points;
+    if(points>= 12)
+        cout<<"Yes";
+    else
+        cout<<"No";
+    return 0;
+}
