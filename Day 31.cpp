c11ee9fb5b83f0f239f52423ac91ebaf50c20037
Harsh_Tripathi <144989081+alwaysharshtripathi@users.noreply.github.com>
2024-11-03 22:34:20 +0530

    Day 31.cpp

---
+++ Day 31.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n, r = 1;
+        cin>>n;
+        r = n * (n - 1);
+        cout<<r<<endl;
+    }
+    return 0;
+}
