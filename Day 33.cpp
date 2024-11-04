478ae32e6a09edba60d7227f6c1cc228ff8f3477
Harsh_Tripathi <144989081+alwaysharshtripathi@users.noreply.github.com>
2024-11-04 23:37:36 +0530

    Day 33.cpp

---
+++ Day 33.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        double r;
+        cin>>n;
+        if( n % 4 == 0) {
+            cout<<"Good"<<endl;
+        }else {
+            cout<<"Not Good"<<endl;
+        }
+    }
+    return 0;
+}
