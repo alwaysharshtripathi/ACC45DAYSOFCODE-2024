05b952495f19428e8bafc98837f2c02ad20b726e
Harsh_Tripathi <144989081+alwaysharshtripathi@users.noreply.github.com>
2024-11-05 23:34:52 +0530

    Day 34.cpp

---
+++ Day 34.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int a, b;
+        cin>>a>>b;
+        if(21 - (a+b) <= 10  && 21  - (a+b) >= 1) {
+            cout<<21 - (a+b)<<endl;
+        }else {
+            cout<<-1<<endl;
+        }
+    }
+    return 0;
+}
